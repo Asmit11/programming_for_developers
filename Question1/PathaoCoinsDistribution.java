04e6a38986d765cb6752ae9618d5aad79ee54db4
Asmit11 <sandeepgahatraj9@gmail.com>
2023-08-11 20:39:07 +0545

    changes made in question number 1(b)

--- Question1/PathaoCoinsDistribution.java
+++ Question1/PathaoCoinsDistribution.java
@@ -1,47 +1,50 @@
 package Question1;
+
 import java.util.Arrays;
 
 public class PathaoCoinsDistribution {
-    public static int getMinimumCoins(int[] ratings) {
-        int n = ratings.length;
+    public static int calculateMinimumCoins(int[] riderRatings) {
+        int n = riderRatings.length;
 
         // Create an array to store the number of coins for each rider
-        int[] coins = new int[n];
+        int[] coinsPerRider = new int[n];
 
         // Initialize the number of coins for each rider as 1
-        Arrays.fill(coins, 1);
+        Arrays.fill(coinsPerRider, 1);
 
         // Traverse from left to right and update the number of coins based on the ratings
         for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
+            if (riderRatings[i] > riderRatings[i - 1]) {
                 // If the current rider has a higher rating than the previous rider, assign one more coin
-                coins[i] = coins[i - 1] + 1;
+                coinsPerRider[i] = coinsPerRider[i - 1] + 1;
             }
         }
 
         // Traverse from right to left and update the number of coins based on the ratings
         for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
+            if (riderRatings[i] > riderRatings[i + 1]) {
                 // If the current rider has a higher rating than the next rider, assign more coins if needed
-                coins[i] = Math.max(coins[i], coins[i + 1] + 1);
+                coinsPerRider[i] = Math.max(coinsPerRider[i], coinsPerRider[i + 1] + 1);
             }
         }
 
         // Calculate the total number of coins required
-        int totalCoins = 0;
-        for (int coin : coins) {
-            totalCoins += coin;
+        int totalCoinsRequired = 0;
+        for (int coins : coinsPerRider) {
+            totalCoinsRequired += coins;
         }
 
-        return totalCoins;
+        return totalCoinsRequired;
     }
 
     public static void main(String[] args) {
-        //  input
-        int[] ratings = {1, 0, 2};
+        // Example input: ratings of riders
+        int[] riderRatings = {1, 0, 2};
 
-        // Call the function to get the minimum number of coins required
-        int minimumCoins = getMinimumCoins(ratings);
-        System.out.println(minimumCoins);
+        // Calculate the minimum number of coins required using the function
+        int minimumCoinsRequired = calculateMinimumCoins(riderRatings);
+        
+        // Display the minimum number of coins required
+        System.out.println("Minimum number of coins required: " + minimumCoinsRequired);
     }
 }
