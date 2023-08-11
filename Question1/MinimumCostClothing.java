82efb2bfef31a38a7c9cb225276f88056f5d4157
Asmit11 <sandeepgahatraj9@gmail.com>
2023-08-11 20:36:20 +0545

    changes in question number 1(a)

--- Question1/MinimumCostClothing.java
+++ Question1/MinimumCostClothing.java
@@ -1,36 +1,39 @@
 package Question1;
+
 public class MinimumCostClothing {
-    public static int getMinimumCost(int N, int[][] price) {
-        // Initialize the minimum cost variables for each person
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
-        int min3 = Integer.MAX_VALUE;
+    public static int calculateMinimumCost(int numberOfSets, int[][] clothingPrices) {
+        // Initialize variables to track the minimum cost for each person
+        int minCostPerson1 = Integer.MAX_VALUE;
+        int minCostPerson2 = Integer.MAX_VALUE;
+        int minCostPerson3 = Integer.MAX_VALUE;
 
         // Iterate over each clothing set
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < numberOfSets; i++) {
             for (int j = 0; j < 3; j++) {
                 // Compare the price of the clothing set with the current minimum cost for each person
                 if (j == 0) {
-                    min1 = Math.min(min1, price[i][j]);
+                    minCostPerson1 = Math.min(minCostPerson1, clothingPrices[i][j]);
                 } else if (j == 1) {
-                    min2 = Math.min(min2, price[i][j]);
+                    minCostPerson2 = Math.min(minCostPerson2, clothingPrices[i][j]);
                 } else {
-                    min3 = Math.min(min3, price[i][j]);
+                    minCostPerson3 = Math.min(minCostPerson3, clothingPrices[i][j]);
                 }
             }
         }
 
-        // Return the total minimum cost
-        return min1 + min2 + min3;
+        // Calculate and return the total minimum cost
+        return minCostPerson1 + minCostPerson2 + minCostPerson3;
     }
 
     public static void main(String[] args) {
-        // Example input
-        int[][] price = {{14, 4, 11}, {11, 14, 3}, {14, 2, 10}};
-        int N = 3;
+        // Example input: prices of clothing sets for each person
+        int[][] clothingPrices = {{14, 4, 11}, {11, 14, 3}, {14, 2, 10}};
+        int numberOfSets = 3;
 
-        // Call the function to get the minimum cost
-        int minimumCost = getMinimumCost(N, price);
-        System.out.println(minimumCost);
+        // Calculate the minimum cost using the function
+        int minimumCost = calculateMinimumCost(numberOfSets, clothingPrices);
+        
+        // Display the minimum cost
+        System.out.println("Minimum total cost for clothing: " + minimumCost);
     }
 }
